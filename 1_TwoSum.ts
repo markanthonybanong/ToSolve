e24948be8fc735b88b2103404afeb2714340ec3a
mark <41400439+markanthonybanong@users.noreply.github.com>
2020-10-11 10:53:42 +0800

    Update 1_TwoSum.ts

--- 1_TwoSum.ts
+++ 1_TwoSum.ts
@@ -4,10 +4,36 @@
  You may assume that each input would have exactly one solution, and you may not use the same element
  twice.
 
- Input: nums = [2,7,11,15], target = 9
+ Input: nums = [7,11,15,2], target = 9
  Output: [0,1]
  Output: Because nums[0] + nums[1] == 9, we return [0, 1]. 
 */
 
-const nums: number[] = [2,7,11,15];
+
+
+function getIndices(numbers: number[], targetNum: number): number[]{
+    const indices: number[] = [];
+    let sumOfTwo: number    = 0;
+    for (let index = 0; index < numbers.length; index++) {
+        for (let index1 = index+1; index1 < numbers.length; index1++) {
+            sumOfTwo = numbers[index] + numbers[index1];
+            if(sumOfTwo === targetNum) {
+                indices.push(index);
+                indices.push(index1);
+                break;
+            }   
+        }
+        if(sumOfTwo === targetNum){
+            break;
+        }
+   
+    }
+    return indices;
+}
+
+console.log(getIndices([7,11,15,2], 9));
+
+
+
+
 
